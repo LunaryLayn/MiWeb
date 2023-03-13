1a1a0c82e7483b1ddf35e869998ccda765c0a57b
LunaryLayn <hupogo2000@protonmail.com>
2023-03-13 16:49:47 +0100

    Hero

--- myjs.js
+++ myjs.js
@@ -5,13 +5,32 @@ $(function () {
 })
 
 //Animacion titulo
-var quoteArray = ["Desarrollador profesional<br>Mobile y Web - Fullstack"];
+var quoteArray = ["Desarrollador profesional - Mobile y Web - Fullstack"];
 var textPosition = 0;
-var speed = 100;
+var speed = 50;
+
+var img = $('<img />', { 
+    src: './res/foto.jpeg', 
+    class: 'hidden-img' // Esta clase oculta la imagen inicialmente
+})
 
 typewriter = () => {
     $('#title').html(quoteArray[0].substring(0, textPosition) + '<span>&#10074</span>') ;
 
-    if (textPosition++ != quoteArray[0].length)
+    if (textPosition++ != quoteArray[0].length) {
         setTimeout(typewriter, speed);
+    } else {
+        $("#mifoto").fadeIn(2000);
+        
+    }
 }
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.navbar').addClass('scrolled');
+    } else {
+      $('.navbar').removeClass('scrolled');
+    }
+  });
+  
+
